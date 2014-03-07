28a5d4c7387e6745bf752a0b9a97f1ae91cd75ea
Sooraj <traeko@Sooraj.(none)>
2014-03-08 00:07:21 +0530

    my first commit

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,7 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# FERGY: We don't need all prebuilt at the moment
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
+# FERGY: This is same as in BoardConfig.mk... Why is doubled here???
 -include vendor/rockchip/rk2928sdk/AndroidBoardVendor.mk
--- cm.mk
+++ cm.mk
@@ -1,4 +1,7 @@
 ## Specify phone tech before including full_phone
+#FERGY: @Sooraj, this is only for GSM version. If You don't have GSM, then use
+# $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+#########################################################################
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
@@ -13,6 +16,6 @@ $(call inherit-product, device/rockchip/rk2928sdk/device_rk2928sdk.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rk2928sdk
 PRODUCT_NAME := cm_rk2928sdk
-PRODUCT_BRAND := rockchip
-PRODUCT_MODEL := rk2928sdk
-PRODUCT_MANUFACTURER := rockchip
+PRODUCT_BRAND := iBall
+PRODUCT_MODEL := slide 6318i
+PRODUCT_MANUFACTURER := iBall
